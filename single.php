e8dcced39548ec3260d7093d8009c360501fd573
Tom Golden <tbjgolden@gmail.com>
2019-02-20 23:41:39 -0800

    Update theme

--- single.php
+++ single.php
@@ -23,15 +23,12 @@ get_header();
 
         get_template_part( 'template-parts/content/content', 'single' );
 
-        if ( is_singular( 'attachment' ) ) {
-          // Parent post navigation.
-          the_post_navigation(
-            array(
-              /* translators: %s: parent post link */
-              'prev_text' => sprintf( __( '<span class="meta-nav">Published in</span><span class="post-title">%s</span>', 'twentynineteen' ), '%title' ),
-            )
-          );
-        } elseif ( is_singular( 'post' ) ) {
+        // If comments are open or we have at least one comment, load up the comment template.
+        if ( comments_open() || get_comments_number() ) {
+          comments_template();
+        }
+
+        if ( is_singular( 'post' ) ) {
           // Previous/next post navigation.
           the_post_navigation(
             array(
@@ -43,11 +40,6 @@ get_header();
                 '<span class="post-title">%title</span>',
             )
           );
-        }
-
-        // If comments are open or we have at least one comment, load up the comment template.
-        if ( comments_open() || get_comments_number() ) {
-          comments_template();
         }
 
       endwhile; // End of the loop.
