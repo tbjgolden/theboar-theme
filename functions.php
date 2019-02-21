6b3a67dbc26ffc4d267d55f84143fe59f41a6fac
Tom Golden <tbjgolden@gmail.com>
2019-02-20 22:23:53 -0800

    Update theme

--- functions.php
+++ functions.php
@@ -1,15 +1,5 @@
 <?php
 /**
- * Twenty Nineteen functions and definitions
- *
- * @link https://developer.wordpress.org/themes/basics/theme-functions/
- *
- * @package WordPress
- * @subpackage Twenty_Nineteen
- * @since 1.0.0
- */
-
-/**
  * Twenty Nineteen only works in WordPress 4.7 or later.
  */
 if ( version_compare( $GLOBALS['wp_version'], '4.7', '<' ) ) {
