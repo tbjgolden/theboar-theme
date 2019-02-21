ab3af0858678a4e09a821b3818a524228a507ff7
Tom Golden <tbjgolden@gmail.com>
2019-02-20 22:31:16 -0800

    Update theme

--- template-parts/content/content-cell.php
+++ template-parts/content/content-cell.php
@@ -1,2 +1,49 @@
 <?php
-echo 'HI';
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
+
+/**
+ * Template part for displaying blocks in main pages
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-hierarchy/
+ *
+ * @package WordPress
+ * @subpackage Twenty_Nineteen
+ * @since 1.0.0
+ */
+
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
+
+$classes = join(' ', array_filter(get_post_class(), function ($class_name) {
+  return $class_name !== 'entry';
+})) . ' cell';
+
+// $post_cats = wp_get_post_categories(get_the_ID());
+// foreach ($post_cats as $cat) {
+//   if ($cat < 15) {
+//     $classes .= ' cell-cat-' . $cat;
+//     return;
+//   }
+// }
+?>
+
+<article id="post-<?php the_ID(); ?>" class="<?php echo $classes; ?>">
+  <figure class="cell-thumbnail">
+    <a class="cell-link" href="<?php echo esc_url( get_permalink() ); ?>" rel="bookmark">
+      <?php
+        $sizes = '(max-width: 606px) calc(100vw - 44px), (max-width: 893px) calc(50vw - 30px), 420px';
+        if ( in_array( $GLOBALS['POST_COUNT'], array(1, 4, 8, 13, 17)) ) {
+          $sizes = '(max-width: 893px) calc(100vw - 44px), 840px';
+        }
+        the_post_thumbnail('medium_large', array( 'sizes' => $sizes ) );
+      ?>
+      <header class="cell-header">
+        <?php the_title('<h2 class="cell-title">', '</h2>'); ?>
+        <div class="cell-content">
+          <?php the_excerpt(); ?>
+        </div><!-- .cell-content -->
+      </header><!-- .cell-header -->
+    </a>
+  </figure><!-- .cell-thumbnail -->
+</article><!-- #post-${ID} -->
