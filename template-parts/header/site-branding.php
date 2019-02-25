b3ca83fb13f1488c49011fe09315c716ae6a8fa6
Tom Golden <tbjgolden@gmail.com>
2019-02-24 19:30:20 -0600

    Update theme

--- template-parts/header/site-branding.php
+++ template-parts/header/site-branding.php
@@ -48,41 +48,6 @@
     </h1>
     ';
   }
-
-  if ( has_nav_menu( 'menu-1' ) ) {
-    echo '<nav id="site-navigation" class="main-navigation';
-
-    if (array_key_exists('CATEGORY', $GLOBALS)) {
-      echo ' in-cat in-cat-' . $GLOBALS['CATEGORY']->term_id;
-    }
-
-    echo '" aria-label="Top Menu">';
-
-    wp_nav_menu(
-      array(
-        'theme_location' => 'menu-1',
-        'menu_class'     => 'main-menu',
-        'items_wrap'     => '<ul id="%1$s" class="%2$s">%3$s</ul>',
-      )
-    );
-
-    echo '</nav><!-- #site-navigation -->';
-  }
-
-  if ( has_nav_menu( 'social' ) ) {
-    echo '<nav class="social-navigation" aria-label="' . esc_attr_e( 'Social Links Menu', 'twentynineteen' ) . '">';
-
-    wp_nav_menu(
-      array(
-        'theme_location' => 'social',
-        'menu_class'     => 'social-links-menu',
-        'link_before'    => '<span class="screen-reader-text">',
-        'link_after'     => '</span>' . twentynineteen_get_icon_svg( 'link' ),
-        'depth'          => 1,
-      )
-    );
-
-    echo '</nav><!-- .social-navigation -->';
-  }
   ?>
+
 </div><!-- .site-branding -->
