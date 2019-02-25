84d5598d2e78d19b8a0128c7fec62af42fad0567
Tom Golden <tbjgolden@gmail.com>
2019-02-24 23:45:33 -0600

    Update theme

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,4 @@
-(function () {
+document.addEventListener("DOMContentLoaded", function(event) {
   [].slice.call((document.querySelectorAll('.entry-content > *') || []), 0).forEach(curr => {
     while (curr && curr.childNodes && curr.childNodes.length) curr = curr.childNodes[0];
     while (curr.length === 1 && curr.parentNode.textContent.length === 1) {
@@ -7,5 +7,4 @@
       parent.insertBefore(x, parent.firstChild);
     }
   });
-  console.log('Hi');
-})();
+});
