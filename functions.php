f43f7d822669b0a797e7cae96bce328909864ed1
Tom Golden <tbjgolden@gmail.com>
2019-02-24 23:24:17 -0600

    Update theme

--- functions.php
+++ functions.php
@@ -221,6 +221,8 @@ function twentynineteen_scripts() {
   if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
     wp_enqueue_script( 'comment-reply' );
   }
+
+  wp_enqueue_script( 'script', get_template_directory_uri() . '/js/scripts.js');
 }
 add_action( 'wp_enqueue_scripts', 'twentynineteen_scripts' );
 
