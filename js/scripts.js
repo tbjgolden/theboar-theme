a667a831eb263650a2ae1f2c1467764fd357cf16
Tom Golden <tbjgolden@gmail.com>
2019-02-24 23:43:43 -0600

    Update theme

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,10 @@
-console.log('Hi');
+(function () {
+  [].slice.call((document.querySelectorAll('.entry-content > *') || []), 0).forEach(curr => {
+    while (curr && curr.childNodes && curr.childNodes.length) curr = curr.childNodes[0];
+    while (curr.length === 1 && curr.parentNode.textContent.length === 1) {
+      var parent = curr.parentNode.parentNode;
+      parent.removeChild(parent.firstChild);
+      parent.insertBefore(x, parent.firstChild);
+    }
+  });
+})();
