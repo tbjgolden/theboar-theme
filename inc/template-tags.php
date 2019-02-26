000e87dd8854fdb9d93076113b5ceed5cd4aa561
Tom Golden <tbjgolden@gmail.com>
2019-02-25 22:20:34 -0600

    Update theme

--- inc/template-tags.php
+++ inc/template-tags.php
@@ -29,7 +29,7 @@ if ( ! function_exists( 'twentynineteen_posted_on' ) ) :
     );
 
     printf(
-      '<span class="posted-on">%1$s%2$s</span>',
+      '<div class="posted-on">%1$s%2$s</div>',
       twentynineteen_get_icon_svg( 'watch', 16 ),
       $time_string
     );
@@ -43,7 +43,7 @@ if ( ! function_exists( 'twentynineteen_posted_by' ) ) :
   function twentynineteen_posted_by() {
     printf(
       /* translators: 1: SVG icon. 2: post author, only visible to screen readers. 3: author link. */
-      '<span class="byline">%1$s<span class="screen-reader-text">%2$s</span><span class="author vcard"><a class="url fn n" href="%3$s">%4$s</a></span></span>',
+      '<div class="byline">%1$s<span class="screen-reader-text">%2$s</span><span class="author vcard"><a class="url fn n" href="%3$s">%4$s</a></span></div>',
       twentynineteen_get_icon_svg( 'person', 16 ),
       __( 'Posted by', 'twentynineteen' ),
       esc_url( get_author_posts_url( get_the_author_meta( 'ID' ) ) ),
