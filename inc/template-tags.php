c1f6fa054a83a42c66ef112e30056fd770c6772b
Tom Golden <tbjgolden@gmail.com>
2019-02-26 22:48:27 -0600

    Update theme

--- inc/template-tags.php
+++ inc/template-tags.php
@@ -46,8 +46,7 @@ if ( ! function_exists( 'twentynineteen_posted_by' ) ) :
   function twentynineteen_posted_by() {
     printf(
       /* translators: 1: SVG icon. 2: post author, only visible to screen readers. 3: author link. */
-      '<div class="byline">%1$s <span class="screen-reader-text">%2$s</span><span class="author vcard"><a class="url fn n" href="%3$s">%4$s</a></span></div>',
-      __( 'Posted by', 'twentynineteen' ),
+      '<div class="byline"><span class="screen-reader-text">Posted by</span><span class="author vcard"><a class="url fn n" href="%1$s">%2$s</a></span></div>',
       esc_url( get_author_posts_url( get_the_author_meta( 'ID' ) ) ),
       esc_html( get_the_author() )
     );
