af6820e28bab79cf5d4f379deaecd8f79fb11526
Tom Golden <tbjgolden@gmail.com>
2019-02-28 19:52:22 -0600

    Update theme

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,20 @@
+window.CategoryColorMap = {
+  'rgb(196, 22, 28)': 'rgb(150, 17, 21)',
+  'rgb(255, 194, 14)': 'rgb(218, 163, 0)',
+  'rgb(242, 101, 34)': 'rgb(213, 77, 13)',
+  'rgb(141, 198, 63)': 'rgb(114, 162, 48)',
+  'rgb(0, 171, 189)': 'rgb(0, 125, 138)',
+  'rgb(237, 28, 36)': 'rgb(198, 16, 23)',
+  'rgb(0, 150, 166)': 'rgb(0, 104, 115)',
+  'rgb(146, 39, 143)': 'rgb(106, 28, 104)',
+  'rgb(121, 0, 32)': 'rgb(70, 0, 19)',
+  'rgb(0, 143, 213)': 'rgb(0, 109, 162)',
+  'rgb(254, 94, 8)': 'rgb(210, 74, 1)',
+  'rgb(255, 0, 255)': 'rgb(204, 0, 204)',
+  'rgb(30, 25, 106)': 'rgb(18, 15, 65)',
+  'rgb(0, 166, 81)': 'rgb(0, 115, 56)'
+};
+
 document.addEventListener("DOMContentLoaded", function(event) {
   // remove user dropcaps
   [].slice.call((document.querySelectorAll('.entry-content > *') || []), 0)
@@ -12,22 +29,6 @@ document.addEventListener("DOMContentLoaded", function(event) {
 
   [].slice.call((document.querySelectorAll('.entry-content [style*="color:"]') || []), 0)
     .forEach(colored => {
-      console.log(colored.style.color);
+      console.log(colored.style.color, window.CategoryColorMap[colored.style.color]);
     });
-
-
-name: news cat-id 1 color rgba(150, 17, 21, 0.5)
-name: comment cat-id 2 color rgba(218, 163, 0, 0.5)
-name: money cat-id 3 color rgba(213, 77, 13, 0.5)
-name: features cat-id 4 color rgba(114, 162, 48, 0.5)
-name: science-tech cat-id 5 color rgba(0, 125, 138, 0.5)
-name: music cat-id 6 color rgba(198, 16, 23, 0.5)
-name: games cat-id 7 color rgba(0, 104, 115, 0.5)
-name: tv cat-id 8 color rgba(106, 28, 104, 0.5)
-name: film cat-id 9 color rgba(70, 0, 19, 0.5)
-name: books cat-id 10 color rgba(0, 109, 162, 0.5)
-name: arts cat-id 11 color rgba(210, 74, 1, 0.5)
-name: lifestyle cat-id 12 color rgba(204, 0, 204, 0.5)
-name: travel cat-id 13 color rgba(18, 15, 65, 0.5)
-name: sport cat-id 14 color rgba(0, 115, 56, 0.5)
 });
