416d23a9febd344baffac610033c50f2f7e00410
Tom Golden <tbjgolden@gmail.com>
2019-02-28 19:47:03 -0600

    Update theme

--- js/scripts.js
+++ js/scripts.js
@@ -14,4 +14,20 @@ document.addEventListener("DOMContentLoaded", function(event) {
     .forEach(colored => {
       console.log(colored.style.color);
     });
+
+
+name: news cat-id 1 color rgba(150, 17, 21, 0.5)
+name: comment cat-id 2 color rgba(218, 163, 0, 0.5)
+name: money cat-id 3 color rgba(213, 77, 13, 0.5)
+name: features cat-id 4 color rgba(114, 162, 48, 0.5)
+name: science-tech cat-id 5 color rgba(0, 125, 138, 0.5)
+name: music cat-id 6 color rgba(198, 16, 23, 0.5)
+name: games cat-id 7 color rgba(0, 104, 115, 0.5)
+name: tv cat-id 8 color rgba(106, 28, 104, 0.5)
+name: film cat-id 9 color rgba(70, 0, 19, 0.5)
+name: books cat-id 10 color rgba(0, 109, 162, 0.5)
+name: arts cat-id 11 color rgba(210, 74, 1, 0.5)
+name: lifestyle cat-id 12 color rgba(204, 0, 204, 0.5)
+name: travel cat-id 13 color rgba(18, 15, 65, 0.5)
+name: sport cat-id 14 color rgba(0, 115, 56, 0.5)
 });
