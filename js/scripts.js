48d28a7cdb7dc73bdeff54053850e1b1edc544d1
Tom Golden <tbjgolden@gmail.com>
2019-02-28 21:11:21 -0600

    Update theme

--- js/scripts.js
+++ js/scripts.js
@@ -1,16 +1,16 @@
+var wordCharRegex = /[A-Za-z0-9]/i
+
 document.addEventListener("DOMContentLoaded", function(event) {
   // remove user dropcaps
   [].slice.call((document.querySelectorAll('.entry-content > *') || []), 0)
-    .forEach(curr => {
+    .forEach((curr, i) => {
       while (curr && curr.childNodes && curr.childNodes.length) curr = curr.childNodes[0];
       var text;
       while (text = curr.parentNode.textContent, curr.length === 1 && text.length === 1) {
         var parent = curr.parentNode.parentNode;
         parent.removeChild(parent.firstChild);
         parent.insertBefore(curr, parent.firstChild);
-
-        console.log({ text });
-        // parent.classList.add('');
+        if (!i && !wordCharRegex.test(text)) parent.classList.add('no-dropcap');
       }
     });
 
