56e4fddb907a5b2904f894afcc139ddab8fca487
Tom Golden <tbjgolden@gmail.com>
2019-02-28 21:05:07 -0600

    Update theme

--- js/scripts.js
+++ js/scripts.js
@@ -3,10 +3,14 @@ document.addEventListener("DOMContentLoaded", function(event) {
   [].slice.call((document.querySelectorAll('.entry-content > *') || []), 0)
     .forEach(curr => {
       while (curr && curr.childNodes && curr.childNodes.length) curr = curr.childNodes[0];
-      while (curr.length === 1 && curr.parentNode.textContent.length === 1) {
+      var text;
+      while (text = curr.parentNode.textContent, curr.length === 1 && text.length === 1) {
         var parent = curr.parentNode.parentNode;
         parent.removeChild(parent.firstChild);
         parent.insertBefore(curr, parent.firstChild);
+
+        console.log({ text });
+        // parent.classList.add('');
       }
     });
 
