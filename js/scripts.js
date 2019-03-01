acf6529dbb884112213f9b4774fb571da9ec45ef
Tom Golden <tbjgolden@gmail.com>
2019-02-28 20:19:21 -0600

    Update theme

--- js/scripts.js
+++ js/scripts.js
@@ -1,18 +1,18 @@
 window.CategoryColorMap = {
-  'rgb(196, 22, 28)': 'rgb(150, 17, 21)',
-  'rgb(255, 194, 14)': 'rgb(218, 163, 0)',
-  'rgb(242, 101, 34)': 'rgb(213, 77, 13)',
-  'rgb(141, 198, 63)': 'rgb(114, 162, 48)',
-  'rgb(0, 171, 189)': 'rgb(0, 125, 138)',
-  'rgb(237, 28, 36)': 'rgb(198, 16, 23)',
-  'rgb(0, 150, 166)': 'rgb(0, 104, 115)',
-  'rgb(146, 39, 143)': 'rgb(106, 28, 104)',
-  'rgb(121, 0, 32)': 'rgb(70, 0, 19)',
-  'rgb(0, 143, 213)': 'rgb(0, 109, 162)',
-  'rgb(254, 94, 8)': 'rgb(210, 74, 1)',
-  'rgb(255, 0, 255)': 'rgb(204, 0, 204)',
-  'rgb(30, 25, 106)': 'rgb(18, 15, 65)',
-  'rgb(0, 166, 81)': 'rgb(0, 115, 56)'
+// 'rgb(196, 22, 28)': 'rgb(150, 17, 21)',
+// 'rgb(255, 194, 14)': 'rgb(218, 163, 0)',
+// 'rgb(242, 101, 34)': 'rgb(213, 77, 13)',
+// 'rgb(141, 198, 63)': 'rgb(114, 162, 48)',
+// 'rgb(0, 171, 189)': 'rgb(0, 125, 138)',
+// 'rgb(237, 28, 36)': 'rgb(198, 16, 23)',
+// 'rgb(0, 150, 166)': 'rgb(0, 104, 115)',
+// 'rgb(146, 39, 143)': 'rgb(106, 28, 104)',
+// 'rgb(121, 0, 32)': 'rgb(70, 0, 19)',
+// 'rgb(0, 143, 213)': 'rgb(0, 109, 162)',
+// 'rgb(254, 94, 8)': 'rgb(210, 74, 1)',
+// 'rgb(255, 0, 255)': 'rgb(204, 0, 204)',
+// 'rgb(30, 25, 106)': 'rgb(18, 15, 65)',
+// 'rgb(0, 166, 81)': 'rgb(0, 115, 56)'
 };
 
 document.addEventListener("DOMContentLoaded", function(event) {
@@ -30,16 +30,20 @@ document.addEventListener("DOMContentLoaded", function(event) {
   // darken category colors
   [].slice.call((document.querySelectorAll('.entry-content [style*="color:"]') || []), 0)
     .forEach(colored => {
-      var oldColor = colored.style.color;
-      var newColor = window.CategoryColorMap[oldColor];
-      if (newColor) {
-        colored.style.color = newColor;
-      } else {
-        // try {
-          var rgb = oldColor.substring(oldColor.indexOf('(') + 1, oldColor.length - 1).split(',').map(s => ~~s);
-          var luminance = 0.2126 * rgb[0] + 0.7152 * rgb[1] + 0.0722 * rgb[2];
-          console.log(luminance);
-        // } catch (e) {}
-      }
+      try {
+        var oldColor = colored.style.color;
+        var rgb = oldColor.substring(oldColor.indexOf('(') + 1, oldColor.length - 1).split(',').map(s => ~~s);
+        var luminance = 0.2126 * rgb[0] + 0.7152 * rgb[1] + 0.0722 * rgb[2];
+        var luminanceDelta = luminance - 128;
+
+        if (luminanceDelta > 0) {
+          var newColor = 'rgb(' + [
+            ~~Math.max(0, rgb[0] - luminanceDelta),
+            ~~Math.max(0, rgb[1] - luminanceDelta),
+            ~~Math.max(0, rgb[2] - luminanceDelta),
+          ].join(',') + ')';
+          colored.style.color = newColor;
+        }
+      } catch (e) {}
     });
 });
