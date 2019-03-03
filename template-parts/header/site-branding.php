09dccb9d8c7959d8613a0c8b3774b8c2b299da08
Tom Golden <tbjgolden@gmail.com>
2019-03-03 10:44:55 -0600

    Update theme

--- template-parts/header/site-branding.php
+++ template-parts/header/site-branding.php
@@ -26,7 +26,7 @@
     if ( ( is_front_page() && is_home() ) || is_category() ) {
       echo '<div class="todays-date">' .
         esc_html( date_i18n( get_option( 'date_format' ), time() ) ) .
-      '</div>';
+      '</div><div class="spacer">  &mdash;  </div>';
     }
 
     echo '<nav id="top-navigation" class="top-navigation" aria-label="Top Menu">' .
