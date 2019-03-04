ac131802e9835ddf53e2fa7657ca44d5fbd44f4f
Tom Golden <tbjgolden@gmail.com>
2019-03-03 20:14:08 -0600

    Update theme

--- template-parts/content/content-excerpt.php
+++ template-parts/content/content-excerpt.php
@@ -14,35 +14,37 @@
 <div class="excerpt" rel="bookmark">
   <?php twentynineteen_post_thumbnail(); ?>
   <a href="<?php echo get_permalink(); ?>">
-    <?php the_title( '<span class="excerpt-title">', '</span>' ); ?>
-    <span class="excerpt-text">
-      <?php the_excerpt( ''); ?>
-    </span><!-- .excerpt-text -->
-    <span class="entry-footer">
-      <?php
-      echo '<span class="byline"><span class="screen-reader-text">Posted by</span><span class="author vcard"><a class="url fn n" href="' .
-        esc_url( get_author_posts_url( get_the_author_meta( 'ID' ) ) ) .
-        '">' .
-        esc_html( get_the_author() ) .
-        '</a></span></span>';
+    <div class="excerpt-text">
+      <?php the_title( '<span class="excerpt-title">', '</span>' ); ?>
+      <span class="excerpt-body">
+        <?php the_excerpt( ''); ?>
+      </span><!-- .excerpt-body -->
+      <span class="entry-footer">
+        <?php
+        echo '<span class="byline"><span class="screen-reader-text">Posted by</span><span class="author vcard"><a class="url fn n" href="' .
+          esc_url( get_author_posts_url( get_the_author_meta( 'ID' ) ) ) .
+          '">' .
+          esc_html( get_the_author() ) .
+          '</a></span></span>';
 
-      echo '<span class="posted-on">' .
-        twentynineteen_get_icon_svg( 'clock', 16 ) .
-        '<span class="screen-reader-text">Published on</span>' .
-        ' <time class="entry-date published" datetime="' . esc_attr( get_the_date( DATE_W3C ) ) . '">' .
-          esc_html( date_i18n( get_option( 'date_format' ), $timestamp ) ) .
-      '</time></span>';
+        echo '<span class="posted-on">' .
+          twentynineteen_get_icon_svg( 'clock', 16 ) .
+          '<span class="screen-reader-text">Published on</span>' .
+          ' <time class="entry-date published" datetime="' . esc_attr( get_the_date( DATE_W3C ) ) . '">' .
+            esc_html( date_i18n( get_option( 'date_format' ), $timestamp ) ) .
+        '</time></span>';
 
-      $categories_list = get_the_category_list( ', ' );
-      if ( $categories_list ) {
-        echo '<span class="cat-links">' .
-          twentynineteen_get_icon_svg( 'archive', 16 ) .
-          ' <span class="screen-reader-text">Posted in</span>' .
-          $categories_list .
-        '</span>';
-      }
-      ?>
-    </span><!-- .entry-footer -->
+        $categories_list = get_the_category_list( ', ' );
+        if ( $categories_list ) {
+          echo '<span class="cat-links">' .
+            twentynineteen_get_icon_svg( 'archive', 16 ) .
+            ' <span class="screen-reader-text">Posted in</span>' .
+            $categories_list .
+          '</span>';
+        }
+        ?>
+      </span><!-- .entry-footer -->
+    </div><!-- .excerpt-text -->
   </a>
 </a>
 
