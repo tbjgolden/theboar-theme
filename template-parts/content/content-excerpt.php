6d4b8d5cf0759b7e3f54e9696ea59fb177be7a84
Tom Golden <tbjgolden@gmail.com>
2019-03-03 20:59:19 -0600

    Update theme

--- template-parts/content/content-excerpt.php
+++ template-parts/content/content-excerpt.php
@@ -12,7 +12,7 @@
 ?>
 
 <div class="excerpt<?php if ( ! twentynineteen_can_show_post_thumbnail() ) { echo ' no-thumbnail'; } ?>" rel="bookmark">
-  <?php twentynineteen_post_thumbnail(); ?>
+  <a class="excerpt-image" href="<?php echo get_permalink(); ?>" style="url('<?php echo get_the_post_thumbnail_url(); ?>')"></a>
   <div class="excerpt-text">
     <?php the_title( '<a class="excerpt-name" href="' . get_permalink() . '">', '</a>' ); ?>
     <span class="excerpt-body">
